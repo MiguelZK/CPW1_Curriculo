9a78eedd8bd916b80319ceef70947ca6b5d49d8f
Miguel Zanona Krasner <73081137+MiguelZK@users.noreply.github.com>
2022-05-06 16:22:24 -0300

    Teste para melhorar ação com imagem (que só funcionava a partir do 2º clique)

--- script/CurriculoMiguel2.js
+++ script/CurriculoMiguel2.js
@@ -1,9 +1,16 @@
 function changeImage(){
-    imagem = document.getElementById('gifMiguel');
+    let imagem = document.getElementById('gifMiguel');
         if (imagem.src.match("Animated")) {
             imagem.src = "imagens/George-Clooney-74101.gif";
         }
         else {
             imagem.src = "imagens/Animated GIF-downsized.gif";
+        }
+    let element = document.getElementById("NomeNaImagem");
+        if (element.value == "Miguel Zanona Krasner") {
+            element.value = element.replace("Miguel Zanona Krasner", "Agora melhorou, né?");
+        }
+        else {
+            element = element.replace("Agora melhorou, né?", "Miguel Zanona Krasner");
         }
     }
