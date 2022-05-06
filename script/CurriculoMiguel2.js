aabe1ebf53af6a2a34127dbf4128dff423794591
Miguel Zanona Krasner <73081137+MiguelZK@users.noreply.github.com>
2022-05-06 18:21:14 -0300

    Alterando código JS e imagem original no HTML

--- script/CurriculoMiguel2.js
+++ script/CurriculoMiguel2.js
@@ -1,9 +1,17 @@
 function changeImage(){
-    imagem = document.getElementById('gifMiguel');
-        if (imagem.src.match("Animated")) {
+    let imagem = document.getElementById('gifMiguel');
+        if (imagem.src.match("Animated GIF-downsized.gif")) {
             imagem.src = "imagens/George-Clooney-74101.gif";
         }
         else {
             imagem.src = "imagens/Animated GIF-downsized.gif";
+        }
+    
+        let element = document.getElementById("NomeNaImagem");
+        if (element.value == "Miguel Zanona Krasner") {
+            element.value = element.replace("Miguel Zanona Krasner", "Agora melhorou, né?");
+        }
+        else {
+            element = element.replace("Agora melhorou, né?", "Miguel Zanona Krasner");
         }
     }
